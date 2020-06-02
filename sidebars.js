f2780662f91b6dba0d689dff99eedfa3fc6ebccc
Gian Calica <calicagian7@gmail.com>
2020-06-02 01:53:28 -1000

    :sparkles: testing-with-different-roles.md

--- sidebars.js
+++ sidebars.js
@@ -22,6 +22,7 @@ module.exports = {
           "radgrad2/developer/coding-standards",
           "radgrad2/developer/testing",
           "radgrad2/developer/e2e-testing",
+          "radgrad2/developer/testing-with-different-roles",
           "radgrad2/developer/building-academic-plans",
           "radgrad2/developer/database-fixtures",
           "radgrad2/developer/continuous-integration",
