1a26344fda8bf421eefc359ba1be86d92c3d0602
Philip Johnson <johnson@hawaii.edu>
2020-11-11 15:25:33 -1000

    UI Architecture Review

--- sidebars.js
+++ sidebars.js
@@ -54,7 +54,7 @@ module.exports = {
             type: 'category',
             label: 'Concepts',
             items: [
-              'developers/design/concepts/ui-architecture',
+              'developers/design/concepts/ui-architecture-review',
               'developers/design/concepts/academic-plans',
               'developers/design/concepts/class-hierarchy',
               'developers/design/concepts/data-model',
