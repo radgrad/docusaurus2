9bf5c492474900f09fe197c88cfecd796702c049
Philip Johnson <johnson@hawaii.edu>
2021-05-14 10:58:00 -1000

    fix home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: <>Learn</>,
+    title: <>For Everyone</>,
     description: (
         <ul>
           <li><a href='/docs/overview/motivation'>Motivation</a></li>
